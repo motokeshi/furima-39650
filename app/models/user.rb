764742c34f0cac86a1bedb1662c16d76062e3ce9
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-08 16:42:49 +0900

    Userモデルのバリデーション修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :nickname,        presence: true
   validates :birthday,        presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze}
   with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
     validates :last_name
     validates :first_name

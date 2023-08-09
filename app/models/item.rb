bdee34d87f80775c85b8d8225d90cb4258044218
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-09 17:56:20 +0900

    itemモデルのpriceにバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
+  validates :price, presence: true, numericality: { in: 300..9999999 }
 end

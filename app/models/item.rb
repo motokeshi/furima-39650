7c17b93c5a23cc26c3fb107e478a439d5d4a529b
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-09 18:26:21 +0900

    active_storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image, dependent: :destroy
+  validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { in: 300..9999999 }

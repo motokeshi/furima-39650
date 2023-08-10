d960ec6bbdf53684616324bc7219a06e4f5fb02e
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-10 10:43:15 +0900

    バリデーション設定ミスの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, presence: true
-  belongs_to :conditon, presence: true
+  belongs_to :category
+  belongs_to :conditon
 
   belongs_to :user
-
   has_one_attached :image, dependent: :destroy
-  validates :image, presence: true
 
-  validates :name, presence: true
+  validates :image,       presence: true
+  validates :name,        presence: true
   validates :description, presence: true
-  validates :price, presence: true, numericality: { in: 300..9999999 }
+  validates :category,    presence: true
+  validates :condition,   presence: true
+  validates :price,       presence: true, numericality: { in: 300..9999999 }
 end

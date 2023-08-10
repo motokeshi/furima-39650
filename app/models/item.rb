6e87c13440035ee25fd9a924375609633dd9c117
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-10 14:47:35 +0900

    itemモデルのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,12 @@ class Item < ApplicationRecord
   has_one_attached :image, dependent: :destroy
 
   validates :image,             presence: true
-  validates :name,              presence: true
-  validates :description,       presence: true
+  validates :name,              presence: true, length: { maximum: 40 }
+  validates :description,       presence: true, length: { maximum: 1000 }
   validates :category,          presence: true
   validates :condition,         presence: true
   validates :shipping_bear,     presence: true
   validates :prefecture,        presence: true
   validates :preparation_date,  presence: true
-  validates :price,             presence: true, numericality: { in: 300..9999999 }
+  validates :price,             presence: true, numericality: { only_integer: true, in: 300..9999999 }
 end

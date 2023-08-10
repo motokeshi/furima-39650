25649ab7bb5dbe4456b6c82d6e8878bfe9924e26
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-10 14:08:50 +0900

    出品時の配送料負担のプルダウン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,16 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :conditon
+  belongs_to :shipping_bear
 
   belongs_to :user
   has_one_attached :image, dependent: :destroy
 
-  validates :image,       presence: true
-  validates :name,        presence: true
-  validates :description, presence: true
-  validates :category,    presence: true
-  validates :condition,   presence: true
-  validates :price,       presence: true, numericality: { in: 300..9999999 }
+  validates :image,         presence: true
+  validates :name,          presence: true
+  validates :description,   presence: true
+  validates :category,      presence: true
+  validates :condition,     presence: true
+  validates :shipping_bear, presence: true
+  validates :price,         presence: true, numericality: { in: 300..9999999 }
 end

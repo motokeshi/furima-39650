dc76e5fd4ced276ab5fa83e1fdf629853baed00f
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-10 09:47:29 +0900

    出品時カテゴリーのプルダウン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, presence: true
+
   belongs_to :user
+  
   has_one_attached :image, dependent: :destroy
   validates :image, presence: true
+
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { in: 300..9999999 }

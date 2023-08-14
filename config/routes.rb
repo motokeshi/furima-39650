fa4ff3bbbf114f0efff609b171a6fde16b6c143b
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-14 15:28:45 +0900

    編集データ保存機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   root "items#index"
 
-  resources :items, only: [:index, :new, :create, :show, :edit]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
 end

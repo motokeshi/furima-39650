11cf9e768dc8b08708e38d9f302fd7145dc6c868
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-14 18:06:28 +0900

    rubocopによる自動整形を実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,9 +39,7 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if item.user_id == current_user.id
-      item.destroy
-    end
+    item.destroy if item.user_id == current_user.id
     redirect_to root_path
   end
 

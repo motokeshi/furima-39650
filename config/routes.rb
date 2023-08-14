48212e11e3bc03b970e7e2953b7efbd1df2f317b
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-14 14:34:02 +0900

    ルーティング、コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   root "items#index"
 
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show, :edit]
 end

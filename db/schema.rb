8c80e6511bb91f7ae464400c57ba102fed02d8d2
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-15 15:01:52 +0900

    addressテーブルのマイグレーション修正

--- db/schema.rb
+++ db/schema.rb
@@ -46,9 +46,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_14_132144) do
     t.string "address", null: false
     t.string "building_name"
     t.string "phone_number", null: false
-    t.string "order", null: false
+    t.bigint "order_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_addresses_on_order_id"
   end
 
   create_table "items", charset: "utf8", force: :cascade do |t|
@@ -95,6 +96,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_14_132144) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "orders"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"

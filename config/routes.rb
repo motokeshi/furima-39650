213b087d6e7d4c71bd258d99c660a08593dbe340
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-15 12:40:25 +0900

    ビューファイルの表示機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root "items#index"
 
-  resources :items
-  resources :orders, only: :index
+  resources :items do
+    resources :orders, only: :index
+  end
 end

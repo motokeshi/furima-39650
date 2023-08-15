806589587c7b1a4890a3a1843d5c54c2e630db39
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-15 15:09:52 +0900

    orderモデルとaddressモデルの保存機能を実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,9 +5,13 @@ class OrderAddress
   validates :user_id, presence: true
   validates :item_id, presence: true
   validates :postal_code, presence: true, format: { with: /\A\d{3}[-]\d{4}\z/ }
-  validates :prefecture_id, presence: true, numericality: { greater_han: 1 }
+  validates :prefecture_id, presence: true, numericality: { greater_than: 1 }
   validates :city, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
 
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 end

9a46e06c27a64b173098f2ceaafe5c3f6a02e365
motokeshi <endless.jazz.waltz@gmail.com>
2023-08-17 12:50:36 +0900

    モデルテストの待機時間を短縮

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe OrderAddress, type: :model do
   before do
     user = FactoryBot.create(:user)
     item = FactoryBot.create(:item)
-    sleep 0.1
+    sleep 0.01
     @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
   end
   describe '商品購入機能' do
